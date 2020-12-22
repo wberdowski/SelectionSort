b4ff4590136cb99ca3a0b1b404babdf2373b7ddd
Wojciech Berdowski <10144015+wberdowski@users.noreply.github.com>
2020-12-22 11:57:07 +0100

    Add colored output.

--- SelectionSort/Source.cpp
+++ SelectionSort/Source.cpp
@@ -14,11 +14,22 @@ int findMinIndex(int* arr, int len, int startIndex) {
 	return minIndex;
 }
 
-void printArray(int* arr, int len) {
+void printArray(int* arr, int len, int currentIdx = -1) {
 	for (int i = 0; i < len; i++) {
+		if(i < currentIdx){
+			cout << "\x1B[32m";
+		} else if (i == currentIdx) {
+			cout << "\x1B[31m";
+		}
+
 		cout << arr[i] << " ";
+
+		if (i == currentIdx) {
+			cout << "\033[0m";
+		}
 	}
-	cout << endl;
+
+	cout << "\033[0m" << endl;
 }
 
 void selectionSort(int* arr, int len) {
@@ -27,9 +38,9 @@ void selectionSort(int* arr, int len) {
 		int minIndex = findMinIndex(arr, len, i);
 
 		if (i != minIndex && current > arr[minIndex]) {
-			cout << "Swap " << arr[i] << " <=> " << arr[minIndex] << endl;
+			printArray(arr, len, i);
+			cout << "\x1B[33mSwap " << arr[i] << " <=> " << arr[minIndex] << "\033[0m\t\t" << endl;
 			swap(arr[i], arr[minIndex]);
-			printArray(arr, len);
 			cout << endl;
 		}
 	}
